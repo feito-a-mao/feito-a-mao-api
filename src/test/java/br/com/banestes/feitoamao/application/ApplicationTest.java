8384e9be2048cf02b063d2346a95ca44a2917cd3
Diego Demétrio <diegodemetrio79@gmail.com>
2023-12-04 22:24:06 -0300

    Atualiza README.md

--- src/test/java/br/com/banestes/feitoamao/application/ApplicationTest.java
+++ src/test/java/br/com/banestes/feitoamao/application/ApplicationTest.java
@@ -3,14 +3,14 @@ package br.com.banestes.feitoamao.application;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import br.com.banestes.feitoamaoapi.FeitoAmaoApiApplication;
+import br.com.banestes.feitoamaoapi.FeitoAMaoApiApplication;
 
 public class ApplicationTest {
 
 
     @Test
     public void testMain() {
-        FeitoAmaoApiApplication app = new FeitoAmaoApiApplication();
+        FeitoAMaoApiApplication app = new FeitoAMaoApiApplication();
         Assert.assertNotNull(app);
     }
 
